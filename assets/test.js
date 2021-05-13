93ced2a3fb321f47abdc8e34cf28196723fce515
Rumtikitum <rt.student.only@gmail.com>
2021-05-12 20:29:07 -0700

    first commit

---
+++ assets/test.js
@@ -0,0 +1,8 @@
+var introPage = document.getElementById('textcontent')
+var questionContent = document.getElementById('questions')
+var btn = document.getElementById('btn')
+
+function startGame() {
+    introPage.classList.add('hide')
+    questionContent.classList.remove('hide')
+}
